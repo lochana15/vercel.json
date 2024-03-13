6164909e14dda526d1463079699d94f6935ed352
dusklight00 <r.rahul.developer@gmail.com>
2024-03-13 20:44:28 +0530

    integrated sms service

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import os
+from twilio.rest import Client
+from flask import Flask, request
 
 app = Flask(__name__)
 
+ACOUNT_SID = "ACedddd78041e2ef37db22ddb717bafeca"
+AUTH_TOKEN = "fe3359325839ac8bf55e31d0daeade2f"
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+client = Client(ACOUNT_SID, AUTH_TOKEN)
+
+
+@app.route("/sms", method=["GET"])
+def sms():
+    to = request.args.get("to")
+    client.messages.create(
+        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+        from_="+15098225799",
+        to=to,
+    )
+    return {"status": "success"}
 
 
 if __name__ == "__main__":
