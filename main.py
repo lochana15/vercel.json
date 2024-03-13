384a5018fa799bd59e4850bba9ac14bf9c3b6401
dusklight00 <r.rahul.developer@gmail.com>
2024-03-13 21:00:49 +0530

    sms integrated

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from flask import Flask, request
 app = Flask(__name__)
 
 ACOUNT_SID = "ACedddd78041e2ef37db22ddb717bafeca"
-AUTH_TOKEN = "fe3359325839ac8bf55e31d0daeade2f"
+AUTH_TOKEN = "94eec8db309590df3bbbdedc89ca86a4"
 
 client = Client(ACOUNT_SID, AUTH_TOKEN)
 
 
-@app.route("/sms", method=["GET"])
+@app.route("/sms", methods=["GET"])
 def sms():
     to = request.args.get("to")
     client.messages.create(
         body="Join Earth's mightiest heroes. Like Kevin Bacon.",
         from_="+15098225799",
-        to=to,
+        to="+918095296336",
     )
     return {"status": "success"}
 
